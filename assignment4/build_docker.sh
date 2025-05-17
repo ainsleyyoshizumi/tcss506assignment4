0db4fc38504fe3043be468c27ac209e88570a20c
Ainsley Yoshizumi <yoshizumiainsley@gmail.com>
2025-05-16 20:24:29 -0700

    new build docker.sh

--- assignment4/build_docker.sh
+++ assignment4/build_docker.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "Building Docker image"
 docker build -t flask-app .
+
+echo "Running the Docker container..."
+docker run -d -p 5000:5000 --name flask-app-container flask-app
+
+echo "Docker contianer is running. Use 'docker ps' to check the status."
